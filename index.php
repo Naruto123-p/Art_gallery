a6d3973020e90fe18ea29b6b0b4ae5fb0b64702c
Kira <71603057+Naruto123-p@users.noreply.github.com>
2022-05-10 23:09:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Artsy</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <label class="logo">Artsy</label>
+        <ul>
+            <li><a href="">Home</a><li>
+            <li><a href="">Contact</a><li>
+            <li><a href="">Arts</a><li>        
+            <li><a href="" class="btn btn-success">login</a><li>     
+
+        </ul>
+
+    </nav>
+    <div class="section1">
+        <label class="img_text">The whole world is an art gallery when you're mindful.</label>
+        <img class="main_img" src="loginup.jpg">
+
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="front2.webp">
+
+            </div>
+            <div class="col-md-8">
+
+            </div>
+
+        </div>
+
+    </div>    
+</body>
+</html>
+
